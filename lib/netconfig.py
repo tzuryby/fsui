7bcef657eb53137bd72d46acd3c067566d8925e6
Tzury Bar Yochay <tzury.by@reguluslabs.com>
2011-07-26 21:30:03 +0300

    netconfig  ui integration

--- lib/netconfig.py
+++ lib/netconfig.py
@@ -10,5 +10,5 @@ def configure():
     for iface, settings in net_conf.iteritems():
         os.system("ifconfig %s up" % iface)
         os.system("ip addr add %s brd + dev %s" % (settings['addr'], iface))
-        if settings.get("gw", None):
+        if settings.get("gw", None):            
             os.system("route add default gw %s dev %s" % (settings['gw'], iface))
