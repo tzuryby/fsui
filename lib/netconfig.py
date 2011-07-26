2d33494da7bd1744679fcc4e6170989bdb8d69e0
Tzury Bar Yochay <tzury.by@reguluslabs.com>
2011-07-26 18:51:19 +0300

    netconfig added, next step, ui integration

--- lib/netconfig.py
+++ lib/netconfig.py
@@ -8,7 +8,7 @@ from conf import appconfig
 def configure():
     conf_tree = appconfig.get()
     for iface in conf_tree["net"]:
-        os.system("ifconfig %s up" % (iface['name'])
+        os.system("ifconfig %s up" % iface['name'])
         os.system("ip addr add %(addr)s brd + dev %(name)s" % iface)
         if iface.get("gw", None):
             os.system("route add default gw %(gw)s dev %(name)s" % iface)
