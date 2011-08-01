9d8471261da4ef721163ff75705beb74fdcecfe6
Tzury Bar Yochay <tzury.by@gmail.com>
2011-08-01 06:06:28 +0300

    conference shown in dashboard

--- lib/conf.py
+++ lib/conf.py
@@ -189,7 +189,7 @@ def get_conference_state():
     }
     
     for room in rooms:
-        users = shell("%s | grep %s | head -n 1" % (FS_CLI_COMMAND % 'show calls', room))
+        users = common.shell("%s | grep %s | head -n 1" % (FS_CLI_COMMAND % 'show calls', room))
         users = users.split(",")
         users = [user for user in users if user in online_users]
         if users:
